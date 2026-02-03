5e19939b9845ad2761b2b2fc963095f6c0799aeb
Luke Wheeler <leverestwheeler@gmail.com>
2026-02-02 19:23:51 -0600

    Initial Commit

---
+++ AIWorkflowCoach/AIWorkflowCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIWorkflowCoachApp.swift
+//  AIWorkflowCoach
+//
+//  Created by school on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct AIWorkflowCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
